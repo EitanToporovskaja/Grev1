768e726bc52d7f890aa4f51474dec8a36b30e9f4
eitan <mail@mail.com>
2024-06-24 12:12:21 -0300

    f

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,4 +8,40 @@ public class HomeController : Controller
     {
         return View();
     }
+    public ActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Login(Usuario model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        bool isValid = ValidateUser(model.nombreUsu, model.contraseña);
+
+        if (isValid)
+        {
+
+            return RedirectToAction("Index", "Home");
+        }
+        else
+        {
+            ModelState.AddModelError("", "Usuario o contraseña incorrectos");
+            return View(model);
+        }
+    }
+
+    /*private bool ValidateUser(string username, string password)
+    {
+        // Aquí debes implementar la lógica para verificar las credenciales en tu base de datos
+        using (var db = new GrevEntities()) // Reemplaza "GrevEntities" con tu contexto de base de datos
+        {
+            var user = db.Usuario.FirstOrDefault(u => u.nombreUsu == username && u.contraseña == password && u.activo);
+            return (user != null);
+        }
+    }*/
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,5 +1,10 @@
-public class Categorias{
-    public int IdCategoria{get; set;}
-    public int IdUsuario{get; set;}
-    public string Nombre {get; set;}
+public class Usuario{
+    public string nombre{get; set;}
+    public string apellido{get; set;}
+    public int idTipoUsuario {get; set;}
+    public string nombreUsu {get; set;}
+    public string contraseña {get; set;}
+    public string empresa {get; set;}
+    
+    public bool activo {get; set;}
 }
